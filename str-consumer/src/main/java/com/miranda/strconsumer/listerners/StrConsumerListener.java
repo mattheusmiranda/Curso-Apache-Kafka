ecb15d7d3f73cf527f62186326eb6ba7d70edd13
matheus <matheusrod59@gmail.com>
2024-06-18 17:02:23 -0300

    record interceptor

--- str-consumer/src/main/java/com/miranda/strconsumer/listerners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/miranda/strconsumer/listerners/StrConsumerListener.java
@@ -2,6 +2,8 @@ package com.miranda.strconsumer.listerners;
 
 import com.miranda.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -18,7 +20,13 @@ public class StrConsumerListener {
         log.info("01 ### Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(
+            groupId = "group-2",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"1"})
+            },
+            containerFactory = "validMessageContainerFactory"
+    )
     public void create2(String message) {
         log.info("02 ### Receive message {}", message);
     }
