89602ad050a456d998dc6f3c1bbd43599f486d92
matheus <matheusrod59@gmail.com>
2024-06-18 14:44:19 -0300

    Adicionado callback no metodo de sendMessage.

--- str-producer/src/main/java/com/miranda/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/miranda/strproducer/services/StringProducerService.java
@@ -1,10 +1,14 @@
 package com.miranda.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -12,6 +16,15 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Sent message with success {}", message);
+                log.info("Partition {}, offset {}", result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+            } else {
+                log.error("Unable to send message=[{}] due to : {}", message, ex.getMessage());
+            }
+        });
     }
 }
