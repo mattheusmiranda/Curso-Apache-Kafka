48b50c311fd35517ba6c634d7022f0526fa654cf
matheus <matheusrod59@gmail.com>
2024-06-18 16:00:52 -0300

    creado custom listener

--- str-consumer/src/main/java/com/miranda/strconsumer/listerners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/miranda/strconsumer/listerners/StrConsumerListener.java
@@ -1,24 +1,26 @@
 package com.miranda.strconsumer.listerners;
 
+import com.miranda.strconsumer.custom.StrConsumerCustomListner;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListner(groupId = "group-1")
     public void create0(String message) {
         log.info("00 ### Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListner(groupId = "group-1")
     public void create1(String message) {
         log.info("01 ### Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListner(groupId = "group-2")
     public void create2(String message) {
         log.info("02 ### Receive message {}", message);
     }
